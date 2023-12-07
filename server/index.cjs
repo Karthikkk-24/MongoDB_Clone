9c9d48e4dd5eee775028a64bcdff1a4710fcc25d
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2023-12-07 23:27:05 +0530

    database fetch done

--- server/index.cjs
+++ server/index.cjs
@@ -1,14 +1,8 @@
-// index.js
-
 const express = require('express');
 const mongoose = require('mongoose');
 
-
 const app = express();
 const PORT = 3000;
-
-
-// Use the routes defined in auth.js
 
 mongoose.connect('mongodb://localhost:27017/designio', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('debug', true);
@@ -18,7 +12,26 @@ app.get('/', (req, res) => {
   res.json(['hello']);
 });
 
-// console.log('Declared routes:', listEndpoints(app));
+// Route to get all databases
+// Route to get all databases
+app.get('/getAllDatabaseList', async (req, res) => {
+  try {
+    // Switch to the 'admin' database
+    const adminDb = mongoose.connection.useDb('admin');
+
+    // Get the list of databases
+    const databaseList = await adminDb.db.admin().listDatabases();
+
+    // Extract only the names of databases
+    const databaseNames = databaseList.databases.map(db => db.name);
+
+    res.json(databaseNames);
+  } catch (error) {
+    console.error('Error getting database list:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 // Start the server
 app.listen(PORT, () => {
