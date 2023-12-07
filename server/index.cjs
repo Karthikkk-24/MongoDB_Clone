1f232ef1cd4f125e2a24a73422a2f3c39033fc02
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2023-12-07 23:35:20 +0530

    database with its collection fetching complete

--- server/index.cjs
+++ server/index.cjs
@@ -33,6 +33,47 @@ app.get('/getAllDatabaseList', async (req, res) => {
 });
 
 
+
+// Route to get all databases with collections
+// Route to get all databases with collections
+// Route to get all databases with collections
+// Route to get all databases with collections
+app.get('/getAllDatabaseListWithCollections', async (req, res) => {
+  try {
+    // Switch to the 'admin' database
+    const adminDb = mongoose.connection.useDb('admin');
+
+    // Get the list of databases
+    const databaseList = await adminDb.db.admin().listDatabases();
+
+    // Extract names of databases
+    const databaseNames = databaseList.databases.map(db => db.name);
+
+    // Fetch collections for each database
+    const databasesWithCollections = await Promise.all(databaseNames.map(async dbName => {
+      const database = mongoose.connection.useDb(dbName); // Switch to the specific database
+
+      // Ensure Mongoose has loaded the models for this database
+      await database.modelNames();
+
+      // Fetch collections using the native driver
+      const collections = await database.db.listCollections().toArray();
+      const collectionNames = collections.map(collection => collection.name);
+      return { [dbName]: collectionNames };
+    }));
+
+    res.json(databasesWithCollections);
+  } catch (error) {
+    console.error('Error getting database list with collections:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
