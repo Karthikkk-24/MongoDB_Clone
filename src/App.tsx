31853aa712e13b5320fcdb17a3a06fc5fad888e1
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-01-01 21:48:26 +0530

    Router Error Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
 import './assets/css/General.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
+import Something from './components/Something';
 
 function App() {
-
   return (
     <>
       <div className='main_container'>
         <div className="left_container">
-            <Sidebar />
+          <Sidebar />
         </div>
         <div className="right_container">
-            <Main />
+          <Router>
+            <Routes>
+              <Route path="/" Component={Main} />
+              <Route path="/main" Component={Something} />
+            </Routes>
+          </Router>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
